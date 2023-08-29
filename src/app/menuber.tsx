8d845af3173c0c0b9ca5c1302baaa0e90e934fe0
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-08-29 12:03:08 +0900

    初期設定、index作成

---
+++ src/app/menuber.tsx
@@ -0,0 +1,17 @@
+export default function Menuber() {
+  return (
+    <div className="main">
+      <div className="leftSide"></div>
+      <div className="link">
+        <li>about</li>
+        <li>production</li>
+        <li>photo</li>
+        <li>contact</li>
+      </div>
+      <div className="rightSide">
+        <p className="name">Hayakawa Miho</p>
+        <img alt="GitHub" src="/githubIcon.png" />
+      </div>
+    </div>
+  );
+}

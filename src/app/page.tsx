ca9544d56ca9c55898bddb40136cd4a298d56ef0
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-08-30 13:31:22 +0900

    index作成、CSS実装 #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import styles from "./styles/page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.sidebar}>
-        <p className={styles.p}>HAYAKAWA MIHO</p>
+        <Link href="/" className={styles.link}>
+          HAYAKAWA MIHO
+        </Link>
       </div>
       <div className={styles.mainVisualBox}>
         <img className={styles.photo} alt="main" src="/main.jpg" />

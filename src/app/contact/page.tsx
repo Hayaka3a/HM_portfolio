cf45405f7681e2696199e021b207c155441c3b24
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-09-19 13:50:54 +0900

    contactページ実装

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,19 @@
 import PageTitle from "@/components/pageTitle";
 import Top from "@/components/top";
 import Form from "./form";
+import styles from "@/app/styles/contact.module.css";
 
 export default function Contact() {
   return (
     <>
       <Top />
       <PageTitle title="contact" />
-      <Form />
+      <div className={styles.main}>
+        <p className={styles.p}>
+          何かございましたら、下記のフォームからご連絡ください。
+        </p>
+        <Form />
+      </div>
     </>
   );
 }

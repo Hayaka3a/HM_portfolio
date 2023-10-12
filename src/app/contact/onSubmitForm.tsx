79870a30a0654f48bf6f3df78e3a0c2cf86cd6b7
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-10-12 12:11:09 +0900

    api/route削除

--- src/app/contact/onSubmitForm.tsx
+++ src/app/contact/onSubmitForm.tsx
@@ -1,10 +1,10 @@
 import { supabase } from "../../createClient.js";
-
 export const onSubmitForm = async (data: any) => {
   const { error } = await supabase.from("contacts").insert(data);
   if (error) {
     console.log(error.code, error.message);
   } else {
     console.log("送信成功");
+    window.location.reload();
   }
 };
